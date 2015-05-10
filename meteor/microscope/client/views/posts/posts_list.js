48ec3a17928b489d08f00b3d079759328057edc7
Ads7 <ads71993@gmail.com>
2015-05-10 13:34:22 +0530

    initial

---
+++ meteor/microscope/client/views/posts/posts_list.js
@@ -0,0 +1,20 @@
+var postsData = [
+{
+title: 'Travelling to Infinity: My Life with Stephen ',
+author: 'jane hawking',
+url: 'http://www.flipkart.com/travelling-infinity-my-life-stephen-english/p/itmdx9vbm5un9vrv?pid=9788184983180&ref=L%3A2722481136069331091&srno=b_1'
+},
+{
+title: 'Dilip Kumar - The Substance and the Shadow ',
+author: 'Kumar Dilip',
+url: 'http://www.flipkart.com/dilip-kumar-substance-shadow-autobiography-english/p/itme349rx3yfndqf?pid=9789381398869&ref=L%3A2722481136069331091&srno=b_3'
+},
+{
+title: 'The Meteor Book',
+author: 'Tom Coleman',
+url: 'http://themeteorbook.com'
+}
+];
+Template.postsList.helpers({
+posts: postsData
+});
